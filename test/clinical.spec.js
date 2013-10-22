7fffe388561fce9f65177d162123a7cf34a5af9d
Jason Hamm <jhamm.business@gmail.com>
2013-10-22 10:54:23 -0400

    Test file

---
+++ test/clinical.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Clinical App', function() {
+
+  beforeEach(module('clinicalApp'));
+
+  describe('clinicalHeader', function() {
+    beforeEach(inject(function($compile) {
+
+    }));
+  });
+});
