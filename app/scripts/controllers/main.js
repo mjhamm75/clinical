10c7ee8ddbaecad1ed2e6c3447386e9b08624ac3
Jason Hamm <jhamm.business@gmail.com>
2013-10-22 09:46:00 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clinicalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
