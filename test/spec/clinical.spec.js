343bcaf8193000d188baba31ab7dbb9659e30b54
Jason Hamm <jhamm.business@gmail.com>
2013-10-22 14:42:29 -0400

    Add formatting to spec

--- test/spec/clinical.spec.js
+++ test/spec/clinical.spec.js
@@ -2,19 +2,19 @@
 
 describe('Clinical App', function() {
 
-  var element, $scope, template;
+  var element, scope, template;
+
   beforeEach(module('clinicalApp'));
 
   describe('clinicalHeader', function() {
     beforeEach(module('app/views/header.html'));
 
     beforeEach(inject(function($templateCache, $rootScope, $compile) {
-      $scope = $rootScope;
+      scope = $rootScope;
       template = $templateCache.get('app/views/header.html');
       $templateCache.put('views/header.html');
-      element = angular.element('<clinical-header></clinical-header>');
       element = $compile(template)($rootScope);
-      $scope.$digest();
+      scope.$digest();
     }));
 
     it('should have a list of links', function() {
