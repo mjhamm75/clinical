78c3df8e57aad94ad2bf8cc989b18bc5b6e6a46f
Dane Russell <dane.russell@availity.com>
2013-10-22 13:06:17 -0400

    adding the encounter controller

---
+++ app/scripts/controllers/encounter.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clinicalApp')
+  .controller('EncounterCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
